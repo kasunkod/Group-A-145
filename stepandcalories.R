6fd35c7d118e29128028bbd743679425e87c2538
kasunkod <kk24adi@herts.ac.uk>
2025-12-08 22:48:50 +0000

    Add files via upload
    Guys, here is the r code so far. Check it and see

---
+++ stepandcalories.R
@@ -0,0 +1,34 @@
+X25 <- read_xlsx("C:/Users/kasun/Desktop/25.xlsx")
+
+dfCalStep <- as.data.frame(X25)
+dfCalStep$Calories <- as.numeric(dfCalStep$calories_burned)
+dfCalStep$Steps <- as.numeric(dfCalStep$step_count)
+
+cor(dfCalStep$Calories, dfCalStep$Steps, use="pairwise.complete.obs", method="spearman")
+
+plot(X25$step_count, X25$calories_burned,
+     main="Step Count vs Calories Burned",
+     sub="Does taking more steps lead to buring more calories?",
+     xlab="Calories Burned",
+     ylab="Step Count",
+     pch=19,
+     col="blue")
+
+
+cor.test(dfCalStep$Steps, dfCalStep$Calories, method="spearman" , exact = FALSE)
+
+hist(X25$calories_burned,
+     breaks = 10,
+     main = "Histogram of Daily Calories Burned",
+     xlab = "Calories Burned",
+     col = "lightblue",
+     border = "black",
+     freq = FALSE)
+
+mean_cal <- mean(X25$calories_burned, na.rm = TRUE)
+sd_cal <- sd(X25$calories_burned, na.rm = TRUE)
+
+curve(dnorm(x, mean = mean_cal, sd = sd_cal),
+      col = "red",
+      lwd = 2,
+      add = TRUE)
